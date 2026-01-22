f1191a9395206d5756c891894fa05c88a9d51cdf
Guillermoanez92 <78507119+Guillermoanez92@users.noreply.github.com>
2026-01-21 20:21:45 -0600

    Update render.py

--- api/render.py
+++ api/render.py
@@ -1,8 +1,8 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Renderer endpoint alive")
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/plain"
+        },
+        "body": "Renderer endpoint alive"
+    }
