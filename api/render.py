b2f5002608458b10c49e67b45189dd8a881f475f
Guillermoanez92 <78507119+Guillermoanez92@users.noreply.github.com>
2026-01-31 19:56:24 -0600

    Delete api/render.py

--- api/render.py
+++
@@ -1,8 +0,0 @@
-def main(request):
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/plain"
-        },
-        "body": "Renderer endpoint alive"
-    }
